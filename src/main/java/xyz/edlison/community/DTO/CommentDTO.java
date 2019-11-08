be34dc344584907196ef23e6e34a648a4e445bbb
Edlison <edlison@126.com>
2019-11-08 18:46:05 +0800

    add comment func

--- src/main/java/xyz/edlison/community/DTO/CommentDTO.java
+++ src/main/java/xyz/edlison/community/DTO/CommentDTO.java
@@ -1,10 +1,17 @@
 package xyz.edlison.community.DTO;
 
 import lombok.Data;
+import xyz.edlison.community.model.User;
 
 @Data
-public class CommentDTO {
+public class CommentDTO {//回复内容
+    private Long id;
     private Long parentId;
-    private String content;
     private Integer type;
+    private Long commentator;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Long likeCount;
+    private String content;
+    private User  user;
 }
